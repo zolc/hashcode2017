17a5aab172739ffd9b2a37af217fa1c02d32b636
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2017-02-25 13:28:50 +0100

    fix test suite script

--- runTestSuite.sh
+++ runTestSuite.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 TEST_SCRIPT_PATH="./runTest.sh"
+PARALLEL_TESTS=0
+
+if [ $# -eq 0 ]; then
+  printf "Usage: $0 {full|simple|example} [-p]\n"
+  printf "The -p flag is for running each task simultaneously\n"
+  exit 1
+fi
 
 # Prepare test suite
 TEST_SUITE="$1"
@@ -17,15 +24,34 @@ case "$TEST_SUITE" in
     ;;
   
   *)
-    printf "Usage: $0 {full|simple|example}\n"
+    printf "Usage: $0 {full|simple|example} [-p]\n"
+    printf "The -p flag is for running each task simultaneously\n"
     exit 1
 esac
 
+# Check if tests should be run in parallel
+if [ $# -eq 2 ]; then
+  if [ "$2" == "-p" ]; then
+    PARALLEL_TESTS=1
+  fi
+fi
+
 # Run tests
-printf "Running ${testcases[*]} tests.\n"
+printf "Running ${testcases[*]} tests "
+if [ $PARALLEL_TESTS -eq 1 ]; then
+  printf "in parallel (multiple processes)"
+else
+  printf "synchronously (one after another)"
+fi
+printf "\n"
+
 for testcase in ${testcases[*]}
 do
-  bash $TEST_SCRIPT_PATH $testcase &
+  if [ $PARALLEL_TESTS -eq 1 ]; then
+    bash $TEST_SCRIPT_PATH $testcase &
+  else
+    bash $TEST_SCRIPT_PATH $testcase
+  fi
 done
 
 wait
